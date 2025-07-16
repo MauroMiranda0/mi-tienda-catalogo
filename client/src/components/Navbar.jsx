3013d9c8b01e5c94f2d347e8b293469187e60473
MauroMiranda0 <maurovergara04@gmail.com>
2025-07-15 21:53:38 -0600

    Repositorio Reiniciado para Otro Proyecto

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { NavLink , Link} from 'react-router-dom';
 import styles from './Navbar.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons';
